82175972dab07e26b81528cb564338c4f27b696c
Mathias-Kabango3 <m.kabango@alustudent.com>
2024-03-03 07:30:26 -0800

    fix bug 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,6 +4,7 @@
 class Square:
     ''' 
     instantiate the class 
+
     '''
 
 
@@ -12,7 +13,7 @@ class Square:
         if not isinstance(size,int):
             raise TypeError('size must be an integer')
         elif size < 0:
-            raise ValueError('Size must be >= 0')
+            raise ValueError('size must be >= 0')
 
         self.__size = size
 
