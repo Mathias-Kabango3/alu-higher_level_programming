216136393e08d732e3e801d9e6c904d17fbbeb3a
Mathias-Kabango3 <m.kabango@alustudent.com>
2024-03-03 07:27:36 -0800

    fix a bug

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -2,9 +2,13 @@
 ''' Create class Square '''
 
 class Square:
-    ''' instantiate the class '''
+    ''' 
+    instantiate the class 
+    '''
 
-    def __init__(self,size):
+
+    def __init__(self,size=0):
+        ''' init size '''
         if not isinstance(size,int):
             raise TypeError('size must be an integer')
         elif size < 0:
