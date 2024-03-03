de51656d21127ba9d25cfa9d6bf1efd4e20276ec
Mathias-Kabango3 <m.kabango@alustudent.com>
2024-03-03 07:23:32 -0800

    exeptions in square

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' Create class Square '''
+
+class Square:
+    ''' instantiate the class '''
+
+    def __init__(self,size):
+        if not isinstance(size,int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('Size must be >= 0')
+
+        self.__size = size
+
