ba3be78e934bec5f593c07e5fe0fe87e3ab33bbf
Mathias-Kabango3 <155973706+Mathias-Kabango3@users.noreply.github.com>
2024-03-23 14:52:46 +0200

    force name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,3 +1,2 @@
---  creates the table force_name on your MySQL server
--- cat 3-force_name.sql | mysql -hlocalhost -uroot -p hbtn_0d_2
+--  creates the table force_name:
 CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
