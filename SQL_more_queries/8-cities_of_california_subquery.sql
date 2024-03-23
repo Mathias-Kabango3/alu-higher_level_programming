3137a6c82b24b505903f0b64834f2a62e81e9609
Mathias-Kabango3 <155973706+Mathias-Kabango3@users.noreply.github.com>
2024-03-23 14:57:08 +0200

    cities of california

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,2 @@
--- lists all the cities of California that can be found in the database hbtn_0d_usa.
--- cat 8-cities_of_california_subquery.sql | mysql -hlocalhost -uroot -p hbtn_0d_usa
+-- lists all the cities of California from hbtn_0d_usa.
 SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California");
