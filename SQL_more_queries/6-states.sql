6c371c0a88748fb870a8d3599b683cfa6871208d
Mathias-Kabango3 <155973706+Mathias-Kabango3@users.noreply.github.com>
2024-03-23 14:55:06 +0200

    states

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,3 @@
--- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
--- cat 6-states.sql | mysql -hlocalhost -uroot -p
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
+-- creates the database hbtn_0d_usa and the table states:
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
