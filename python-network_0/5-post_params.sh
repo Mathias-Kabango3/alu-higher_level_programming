91bfc8b12a7a65ab7648d19fb9ee2a5884b4464a
Mathias-Kabango3 <m.kabango@alustudent.com>
2024-03-24 11:26:59 -0700

    post params

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#make a post request with email an subject as params
-curl -s -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+# make post request 
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
