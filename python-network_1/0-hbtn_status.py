edb58abc6c0aa4ff62667f0d814ba2e23897237b
Mathias-Kabango3 <m.kabango@alustudent.com>
2024-03-24 12:11:47 -0700

    status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import urllib
+""" Write a Python script that fetches https://intranet.hbtn.io/status """
 
-'''script that feches all he url'''
-#use contex manager
+import urllib.request
 
-with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
-     content = response.read()
-     print("Body response:")
-     print(f"\t- type: {type(content)}")
-     print(f"\t- content: {content}")
-     print(f"\t- utf8 content: {content.decode('utf-8')}")
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+
+    print("Body response:\n\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode("utf-8")))
