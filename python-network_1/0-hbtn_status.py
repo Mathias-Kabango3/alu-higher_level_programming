7dceaf6729598d3069b4f98f5760c500bb103b97
Mathias-Kabango3 <m.kabango@alustudent.com>
2024-03-24 11:46:59 -0700

    status

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' script that fetches a url'''
+
+
+import urllib.request
+
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {content.decode('utf-8')}")
