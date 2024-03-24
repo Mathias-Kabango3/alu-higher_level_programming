ce704eb3217a10d2cfe99211c1fb740a7aee49e8
Mathias-Kabango3 <m.kabango@alustudent.com>
2024-03-24 11:55:22 -0700

    status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,12 +3,12 @@
 
 
 import urllib.request
-
+"""use contex manager"""
 
 if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
         print("Body response:")
-        print(f"\t- type: {type(content)}")
-        print(f"\t- content: {content}")
-        print(f"\t- utf8 content: {content.decode('utf-8')}")
+        print(f"  - type: {type(content)}")
+        print(f"  - content: {content}")
+        print(f"  - utf8 content: {content.decode('utf-8')}")
