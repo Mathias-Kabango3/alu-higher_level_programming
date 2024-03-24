623d3fa44ba0338229f495b784b48856e57554ab
Mathias-Kabango3 <m.kabango@alustudent.com>
2024-03-24 12:22:27 -0700

    header

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,6 +5,7 @@ import urllib.request
 import sys
 
 
-if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        print(response.info().get('X-Request-Id'))
+if __name__ == '__main__':
+    with urllib.request.urlopen(sys.argv[1]) as res:
+        header = res.info()
+        print(header['X-Request-Id'])
