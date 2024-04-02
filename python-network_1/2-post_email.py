14bb040495813203840727deea9f0d9c90e6c213
Mathias-Kabango3 <m.kabango@alustudent.com>
2024-04-02 09:20:00 -0700

    post email

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Post email
+"""
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as res:
+        print(res.read().decode("utf-8"))
