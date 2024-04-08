d28e5b48edf0a3ec3c78479224a781c9f0556b5c
Mathias-Kabango3 <m.kabango@alustudent.com>
2024-04-08 01:44:01 -0700

    add a and b

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+''' Function (add_interger) that adds two numbers and return the result'''
+
+def add_integer(a,b=98):
+     ''' Function that adds two integers
+    Args:
+        a : this must be either an integer or float
+        b : Must be either an integer or float, and if not provided
+            it takes the defualt value of 98
+    Returns:
+        an integer: the addition of a and b
+    '''
+    if type(a) == float or type(b) == float:
+        a = int(a)
+        b = int(b)
+
+    if type(a) != int:
+        raise TypeError("a must be an integer")
+    if type(b) != int:
+        raise TypeError("b must be an integer")
+    result = a+b
+    return result
