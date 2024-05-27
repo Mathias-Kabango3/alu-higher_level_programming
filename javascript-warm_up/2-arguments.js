65fd943cf24aeb677ab219acd4c365386ca22ee0
Mathias-Kabango3 <m.kabango@alustudent.com>
2024-05-27 16:39:31 +0000

    fix bug

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const argument = process.argv.slice(2)
+const argument = process.argv.slice(2);
 if(argument.length === 0){
 	console.log('No argument');
-}
-else if(argument.length === 1){
+} else if(argument.length === 1){
 	console.log('Argument found');
 }
 else{
