cf3ed5e89f17028b4c9741999ef8f427dc2356b8
Mathias-Kabango3 <m.kabango@alustudent.com>
2024-05-30 09:56:50 +0000

    fix bug

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const languages = ['C is fun','Python is cool','JavaScript is amazing'];
-for(lang of languages){
-	console.log(lang);
+while(let i=0;i<languages.length;i++){
+	console.log(languages[i]);
 }
